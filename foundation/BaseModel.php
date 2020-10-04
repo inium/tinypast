7161d6bf5e510f8e94ebba7fae9e53c2edfbe42a
inlee <einable@gmail.com>
2020-10-04 15:36:15 +0900

    refactor: PDO Wrapper 구현 후 모델에서 분리 및 코드 수정

--- foundation/BaseModel.php
+++ foundation/BaseModel.php
@@ -7,16 +7,14 @@
 
 namespace Foundation;
 
-use \PDO;
-
 class BaseModel
 {
     /**
-     * PDO Instance
+     * Database Instance
      *
-     * @var \PDO
+     * @var \Foundation\Database
      */
-    protected static $pdo = null;
+    protected $database = null;
 
     /**
      * 테이블 이름
@@ -32,65 +30,8 @@ class BaseModel
      */
     public function __construct($tableName)
     {
-        // create pdo instance
-        if (is_null(self::$pdo)) {
-            // .env에 정의된 데이터를 가져온다.
-            // /public/index.php의 Foundation\DotEnv 모듈에서 불러온 값을 사용.
-            $conn = (object) array(
-                'host' => getenv('DB_HOST'),
-                'user' => getenv('DB_USER'),
-                'pass' => getenv('DB_PASS'),
-                'name' => getenv('DB_NAME'),
-                'port' => getenv('DB_PORT') ? getenv('DB_PORT') : 3306,
-                'charset' => getenv('DB_CHARSET')
-                    ? getenv('DB_CHARSET')
-                    : 'utf8'
-            );
-
-            $this->createConnection(
-                $conn->host,
-                $conn->user,
-                $conn->pass,
-                $conn->name,
-                $conn->port,
-                $conn->charset
-            );
-        }
-
         $this->tableName = $tableName;
-    }
-
-    /**
-     * PDO static instance를 생성한다.
-     *
-     * @param string $host      MySQL host
-     * @param string $user      MySQL user name
-     * @param string $pass      MySQL user password
-     * @param string $dbnam     MySQL database name
-     * @param integer $port     MySQL Port number. Default is 3306.
-     * @param string $charset   MySQL Charset. Default is utf8.
-     */
-    private function createConnection(
-        $host,
-        $user,
-        $pass,
-        $dbname,
-        $port = 3306,
-        $charset = 'utf8'
-    ) {
-        try {
-            $options = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-            );
-
-            $dsn = "mysql:host={$host};dbname={$dbname}";
-            self::$pdo = new \PDO($dsn, $user, $pass, $options);
-            self::$pdo->exec("SET NAMES {$charset}");
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            exit();
-        }
+        $this->database = \Foundation\Database::getInstance();
     }
 
     /**
@@ -103,7 +44,7 @@ class BaseModel
         $tableName = $this->tableName();
         $query = "SELECT * FROM {$tableName}";
 
-        $stmt = $this->query($query);
+        $stmt = $this->database->query($query);
 
         return $stmt->fetchAll();
     }
@@ -119,7 +60,7 @@ class BaseModel
         $tableName = $this->tableName();
         $query = "SELECT * FROM {$tableName} WHERE id = ?";
 
-        $stmt = $this->query($query, array($id));
+        $stmt = $this->database->query($query, array($id));
 
         return $stmt->fetch();
     }
@@ -148,8 +89,9 @@ class BaseModel
         $query = "INSERT INTO {$tableName} ({$insertColumns})
                          VALUES ({$valueQuestions})";
 
-        $stmt = $this->query($query, $values);
-        return self::$pdo->lastInsertId();
+        $stmt = $this->database->query($query, $values);
+
+        return $this->database->pdo()->lastInsertId();
     }
 
     /**
@@ -183,7 +125,7 @@ class BaseModel
 
         // id 추가
         array_push($values, $id);
-        $stmt = $this->query($query, $values);
+        $stmt = $this->database->query($query, $values);
 
         return $stmt->rowCount();
     }
@@ -199,34 +141,9 @@ class BaseModel
         $tableName = $this->tableName();
         $query = "DELETE FROM {$tableName} WHERE id = ?";
 
-        $stmt = $this->query($query, array($id));
+        $stmt = $this->database->query($query, array($id));
 
         return $stmt->rowCount();
-    }
-
-    /**
-     * 질의한 결과를 반환한다.
-     * -------------------------------------------------------------------------
-     * $query = "SELECT * FROM fruit WHERE calories < ? AND colour = ?";
-     * $param = array(150, 'red');
-     *
-     * $object->query($query, $param);
-     * -------------------------------------------------------------------------
-     * @param string $query     SQL Query
-     * @param array $param      Query Parameters
-     * @return \PDOStatement    PDOStatement
-     */
-    protected function query($query, $param = array())
-    {
-        try {
-            $stmt = self::$pdo->prepare($query);
-            $stmt->execute($param);
-
-            return $stmt; // return $stmt->fetchAll();
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            exit();
-        }
     }
 
     /**
