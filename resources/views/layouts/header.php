36e6628bbab7c2d33b8e82c603e2a864bb513824
inlee <einable@gmail.com>
2020-10-11 19:22:06 +0900

    refactor: title 수정

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Thin framework for the lower php version (5.3.8)">
     <meta name="author" content="inlee">
-    <title>Thin framework for the lower php version</title>
+    <title>tinypast for the lower php version</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="/public/css/app.css">
